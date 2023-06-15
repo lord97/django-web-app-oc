7c98dbd2de181c5cbf8f9921acafa9fdbe3608f8
lord97 <sanoumohamed97@gmail.com>
2023-06-15 23:52:21 +0000

    add admin page

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band, Listing
+
+
+class BandAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('name', 'year_formed', 'genre')
+
+class ListingAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('title', 'sold', 'types','band')
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing,ListingAdmin)
