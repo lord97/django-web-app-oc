77758d13087a9ed007c49a306326392d942119de
lord97 <sanoumohamed97@gmail.com>
2023-06-19 01:05:53 +0000

    Crup page in progress

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
 from listings.models import Band,Listing
-
-def hello(request) :
+from listings.forms import ContactUsForm
+def band_list(request) :
 
     bands = Band.objects.all()
-    return render(request,'listings/hello.html',{'bands':bands})
+    return render(request,'listings/band_list.html',{'bands':bands})
+
+def band_detail(request, id) :
+
+    try:
+        band = Band.objects.get(id=id)
+        return render(request, 'listings/band_detail.html', {'band': band})
+    except Band.DoesNotExist:
+        return HttpResponse("Cette page n'existe pas")
 
 def about(request) :
     return render(request,'listings/about.html')
 
 
-def listings(request) :
+def listing_list(request) :
     listings = Listing.objects.all()
-    return render(request,'listings/listing.html',{'listings':listings})
+    return render(request,'listings/listing_list.html',{'listings':listings})
+
+def listing_detail(request, id) :
+
+    try:
+        listing = Listing.objects.get(id=id)
+        return render(request, 'listings/listing_detail.html', {'listing': listing})
+    except Band.DoesNotExist:
+        return HttpResponse("Cette page n'existe pas")
 
 def contact(request) :
-    return render(request,'listings/contacts.html')
+    form = ContactUsForm()
+    return render(request,'listings/contacts.html', {'form' : form})
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -20,8 +20,10 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:id>/', views.band_detail, name='band-detail'),
     path('about-us/', views.about),
-    path('listings/', views.listings),
-    path('contact-us/', views.contact),
+    path('listings/', views.listing_list, name ="annonces"),
+    path('listings/<int:id>/', views.listing_detail, name ="annonce-detail"),
+    path('contact-us/', views.contact, name='contact'),
 ]
